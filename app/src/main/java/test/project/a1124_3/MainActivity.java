dd38904ae92ed07aa1a861253f7e77d17b6117a3
GitTest <v304245@yahoo.com.tw>
2016-11-24 11:33:24 +0800

    1

--- app/src/main/java/test/project/a1124_3/MainActivity.java
+++ app/src/main/java/test/project/a1124_3/MainActivity.java
@@ -4,13 +4,15 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
         MyTask task = new MyTask();
         task.execute(5);
     }
@@ -23,13 +25,26 @@ public class MainActivity extends AppCompatActivity {
             for (int i=0;i<=n;i++)
             {
                 Log.d("Task", String.valueOf(i));
+                publishProgress(i);
                 try{
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            return null;
+            return 100;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            tv.setText(String.valueOf(values[0]));
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            tv.setText(String.valueOf(integer));
         }
     }
 }
